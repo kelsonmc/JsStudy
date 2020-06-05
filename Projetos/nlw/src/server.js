9394057621184fc94ddac492b171ff36f0f3bd97
Kelson <kelsonmendescorrea@gmail.com>
2020-06-04 22:21:10 -0300

    Back End Project

---
+++ Projetos/nlw/src/server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const server = express()
+//configurar pasta publica
+server.use(express.static("public"))
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+//configurar caminhos
+// pagina inicial
+// req : requisição
+// res : resposta
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+server.get("/search-results", (req, res) => {
+    return res.render("search-results.html")
+})
+
+//ligar o servidor
+server.listen(3000)
